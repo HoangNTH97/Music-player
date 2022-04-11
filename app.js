e57b43f99d57ce15b5bc5d2e29f62e34c525505c
Hoang Nguyen <infamousingamine@gmail.com>
2022-04-11 19:55:48 +0700

    update

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ const musics = [
   {
     id: 1,
     title: "Loser",
-    singer: "BIGBAng",
+    singer: "BIGBANG",
     src: "Loser-BIGBANG-6291940",
     image:
       "https://images.unsplash.com/photo-1511525499366-bc3f823bacb7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
